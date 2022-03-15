3077ca18e0bc385fc8ac773b9efc2cf3ae4b4b97
Narendra Deshmukh <49844678+Narendra28D@users.noreply.github.com>
2022-03-15 22:22:16 +0530

    ND | Added changes to check the game status and notifying the winner

--- src/app/cell/cell.component.ts
+++ src/app/cell/cell.component.ts
@@ -1,3 +1,5 @@
+import { Player } from './../model/Player.model';
+import { GameStatus } from './../model/GameStatus';
 import { CellNumberGeneratorService } from './../services/cell-number-generator.service';
 import { GameBoardService } from './../services/game-board.service';
 import { PlayerTurnService } from './../services/player-turn.service';
@@ -18,28 +20,37 @@ export class CellComponent implements OnInit {
     private gameBoardService: GameBoardService,
     private cellNumberGeneratorService: CellNumberGeneratorService
   ) {
-     let cellPosition: number[] = cellNumberGeneratorService.getCellPosition();
-     this.r = cellPosition[0];
-     this.c = cellPosition[1];
+    let cellPosition: number[] = cellNumberGeneratorService.getCellPosition();
+    this.r = cellPosition[0];
+    this.c = cellPosition[1];
   }
 
   @HostListener('click') onClick() {
-    if (this.cellValue === ' ') {
-      this.cellValue = this.playerTurnService.getNextTurn().symbol;
-      this.gameBoardService.updatPosition(
-        this.r,
-        this.c,
-        this.cellValue
-      );
+    if (
+      this.cellValue === ' ' &&
+      GameStatus.IN_PROGRESS == this.gameBoardService.getGameStatus()
+    ) {
+      let player = this.playerTurnService.getNextTurn();
+      this.cellValue = player.symbol;
+      this.gameBoardService.updatPosition(this.r, this.c, this.cellValue);
       console.log(
-        'updated cell [' +
-          (this.r ) +
-          '] [' +
-          (this.c ) +
-          '] with ' +
-          this.cellValue
+        'updated cell [' + this.r + '] [' + this.c + '] with ' + this.cellValue
       );
-      console.log(' Game status ' + this.gameBoardService.getGameStatus())
+      this.processGameStatus(player);
+    }
+  }
+
+  private processGameStatus(player: Player): void {
+    let gameStatus = this.gameBoardService.getGameStatus();
+    switch (gameStatus) {
+      case GameStatus.WON: {
+        alert(player.name + ' is winner');
+        break;
+      }
+      case GameStatus.DRAWN: {
+        alert('game completed without result');
+        break;
+      }
     }
   }
 
